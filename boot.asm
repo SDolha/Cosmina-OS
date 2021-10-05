73a349b12caec5179500a10d284ad55b63e10b9c
Sorin Dolha <sdolha@hotmail.com>
2021-10-05 20:00:08 +0300

    Use loop

--- boot.asm
+++ boot.asm
@@ -1,43 +1,16 @@
+org 0x7c00
+mov ax, cs
+mov es, ax
+mov si, msg
 mov ah, 0x0e
-mov al, 'C'
+print:
+mov al, [si]
 int 0x10
-mov al, 'o'
-int 0x10
-mov al, 's'
-int 0x10
-mov al, 'm'
-int 0x10
-mov al, 'i'
-int 0x10
-mov al, 'n'
-int 0x10
-mov al, 'a'
-int 0x10
-mov al, ' '
-int 0x10
-mov al, '1'
-int 0x10
-mov al, '.'
-int 0x10
-mov al, '0'
-int 0x10
-mov al, 13
-int 0x10
-mov al, 10
-int 0x10
-mov al, 'S'
-int 0x10
-mov al, 'a'
-int 0x10
-mov al, 'l'
-int 0x10
-mov al, 'u'
-int 0x10
-int 0x10
-mov al, 't'
-int 0x10
-mov al, '!'
-int 0x10
+inc si
+cmp si, end
+jnz print
 jmp $
-times 510 - ($-$$) db 0
+msg: db `Cosmina 1.0.1\n\rSaluut!`
+end:
+times 510-($-$$) db 0
 dw 0xaa55
